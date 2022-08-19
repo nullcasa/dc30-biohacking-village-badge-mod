638985835628a20410c9059ab53770fce223482d
nullcasa <contact@null.casa>
2022-08-19 17:33:12 -0400

    Final firmware

--- src/main.c
+++ src/main.c
@@ -7,16 +7,31 @@
 #include <util/delay.h>
 
 
+void buzzer_on(void) {
+  TCCR1 = (1 << 2) | (1 << 1);
+  GTCCR = (1 << 6) | (1 << 5);
+  OCR1B = 60;
+  OCR1C = 120;
+}
+
+
+void buzzer_off(void) {
+  GTCCR = 0;
+}
+
+
 int main(void) {
-  DDRB = (1 << PB2) | (1 << PB1) | (1 << PB0);
+  DDRB = (1 << PB4) | (1 << PB2) | (1 << PB1) | (1 << PB0);
 
   PORTB = (1 << PB2) | (1 << PB1) | (1 << PB0);
 
   bool input_state = false;
   uint8_t health = 3;
 
+  buzzer_off();
+
   while(1) {
-    if(PINB & (1 << PB4)) {
+    if(PINB & (1 << PB3)) {
       if(input_state == false) {
         input_state = true;
         health--;
@@ -33,13 +48,18 @@ int main(void) {
           PORTB &= ~(1 << PB0);
         }
 
-        _delay_ms(500);
+        buzzer_on();
+        _delay_ms(100);
+        buzzer_off();
+        _delay_ms(400);
       }
     } else {
       input_state = false;
     }
 
     if(health < 1) {
+      buzzer_on();
+
       PORTB = (1 << PB2) | (1 << PB1) | (1 << PB0);
       _delay_ms(250);
       PORTB = 0;
@@ -62,6 +82,8 @@ int main(void) {
 
       PORTB = (1 << PB2) | (1 << PB1) | (1 << PB0);
       health = 3;
+
+      buzzer_off();
     }
   }
 
